98e027d66ec318dc4d78f20d124b9bb90bb3aaaf
Dhriti12 <71137659+Dhriti12@users.noreply.github.com>
2020-10-23 14:00:38 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,34 @@
+class Form {
+    constructor() {
+      
+    }
+  
+    display(){
+      var title = createElement('h2')
+      title.html("Car Racing Game");
+      title.position(500, 100);
+      
+      var input = createInput("Name");
+      var button = createButton('Play');
+      
+      input.position(500, 200);
+      button.position(550, 250);
+  
+      button.mousePressed(function(){
+        input.hide();
+        button.hide();
+  
+        var name = input.value();
+        
+        playerCount+=1;
+        player.update(name)
+        player.updateCount(playerCount);
+        var greeting = createElement('h3');
+        greeting.html("Hello " + name )
+        greeting.position(500, 200)
+      });
+  
+    }
+  }
+
+
